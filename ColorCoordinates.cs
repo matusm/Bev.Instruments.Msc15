4ab325893bcf3e701cb8a77e6c15b5e44f85478f
Michael Matus <michael@matus.at>
2022-09-08 12:17:05 +0200

    Streamline ColorCoordinates

--- ColorCoordinates.cs
+++ ColorCoordinates.cs
@@ -2,20 +2,15 @@
 {
     public class ColorCoordinates
     {
-        public double X { get; }
-        public double Y { get; }
-        public double Z { get; }
+        public double X { get; } = double.NaN;
+        public double Y { get; } = double.NaN;
+        public double Z { get; } = double.NaN;
         public double x => X / (X + Y + Z);
         public double y => Y / (X + Y + Z);
         public double uPrime => 4 * X / (X + 15 * Y + 3 * Z);
         public double vPrime => 9 * Y / (X + 15 * Y + 3 * Z);
 
-        public ColorCoordinates()
-        {
-            X = double.NaN;
-            Y = double.NaN;
-            Z = double.NaN;
-        }
+        public ColorCoordinates() { } // for undefined value
 
         public ColorCoordinates(double X, double Y, double Z)
         {
